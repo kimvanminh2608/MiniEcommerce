90e03fa78f1b5f210dcb5723d1c53ace73968177
Kim Van Minh <kimvanminh2608@gmail.com>
2024-07-11 14:59:36 +0700

    add extensions customize program.cs

---
+++ Web-Microservice/src/Services/Product.API/Extensions/ConfigureHostExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Product.API.Extensions
+{
+    public static class ConfigureHostExtensions
+    {
+        public static void AddAppConfiguration(this ConfigureHostBuilder host)
+        {
+            host.ConfigureAppConfiguration((context, config) =>
+            {
+                var env = context.HostingEnvironment;
+                config.AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appSettings.{env.EnvironmentName}.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            });
+        }
+    }
+}
