3a5632ada92fe3899ecfe205a1c265faf9f46d42
Kim Van Minh <kimvanminh2608@gmail.com>
2024-07-24 12:12:17 +0700

    Add IntergrationEvent

---
+++ Web-Microservice/src/BuildingBlocks/EventBus/EventBus.Messages/IntergrationEvent/Events/BasketCheckOutEvent.cs
@@ -0,0 +1,20 @@
+﻿using EventBus.Messages.IntergrationEvent.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.IntergrationEvent.Events
+{
+    public record BasketCheckOutEvent() : IntergrationBaseEvent, IBasketCheckoutEvent
+    {
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string ShippingAddress { get; set; }
+        public string InvoiceAddress { get; set; }
+    }
+}
