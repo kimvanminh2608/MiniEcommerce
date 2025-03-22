157c8a40b7707aad0e562368299acb8736bb4245
kimvanminh2608 <kimvanminh2608@gmail.com>
2025-03-22 15:29:40 +0700

    Add inventory grpc

--- Web-Microservice/src/Services/Inventory.Grpc/Program.cs
+++ Web-Microservice/src/Services/Inventory.Grpc/Program.cs
@@ -2,6 +2,7 @@
 
 using Common.Logging;
 using Inventory.Grpc.Extensions;
+using Inventory.Grpc.Services;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,7 +19,7 @@ try
     var app = builder.Build();
 
     // Configure the HTTP request pipeline.
-    //app.MapGrpcService<GreeterService>();
+    app.MapGrpcService<InventoryService>();
     app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
     app.Run();
---
+++ Web-Microservice/src/Services/Inventory.Grpc/Services/InventoryService.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+using Inventory.Grpc.Protos;
+using Inventory.Grpc.Repositories.Interfaces;
+using System;
+using Ilogger = Serilog.ILogger;
+namespace Inventory.Grpc.Services
+{
+    public class InventoryService : StockProtoService.StockProtoServiceBase
+    {
+        private readonly IInventoryRepository _repository;
+        private readonly Ilogger _logger;
+        public InventoryService(IInventoryRepository repository, Ilogger logger)
+        {
+            _repository = repository ?? throw new ArgumentException(nameof(repository));
+            _logger = logger ?? throw new ArgumentException(nameof(logger));
+        }
+
+        override public async Task<StockModel> GetStock(GetStockRequest request, ServerCallContext context)
+        {
+            _logger.Information($"Begin get stock of itemno {request.ItemNo}");
+            var stockQuantity = await _repository.GetStockQuantity(request.ItemNo);
+            var result = new StockModel { Quantity = stockQuantity };
+            _logger.Information($"End get stock of itemno {request.ItemNo} - quantity: {result.Quantity}");
+            return result;
+        }
+    }
+}
