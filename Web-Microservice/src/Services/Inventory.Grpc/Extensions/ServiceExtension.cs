747234531dccc0d7b1f8d4bb57f23e7cb84efbcf
kimvanminh2608 <kimvanminh2608@gmail.com>
2025-03-22 14:29:05 +0700

    set up infrastructure

---
+++ Web-Microservice/src/Services/Inventory.Grpc/Extensions/ServiceExtension.cs
@@ -0,0 +1,43 @@
+﻿using Infrastructure.Extensions;
+using Inventory.Grpc.Repositories;
+using Inventory.Grpc.Repositories.Interfaces;
+using MongoDB.Driver;
+using Shared.Configurations;
+
+namespace Inventory.Grpc.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static void AddInfrastructureServices(this IServiceCollection services)
+        {
+            //services.AddAutoMapper(cfg => cfg.AddProfile(new MappingProfile()));
+            services.AddScoped<IInventoryRepository, InventoryRepository>();
+        }
+
+        internal static IServiceCollection AddConfigurationSettings(this IServiceCollection services, IConfiguration configuration)
+        {
+            var dataSettings = configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+            services.AddSingleton(dataSettings);
+            return services;
+        }
+
+        public static void ConfigureMongoDbClient(this IServiceCollection services)
+        {
+            services.AddSingleton<IMongoClient>(new MongoClient(GetMongoConnectionString(services)))
+                .AddScoped(x => x.GetService<IMongoClient>()?.StartSession());
+        }
+
+        private static string GetMongoConnectionString(this IServiceCollection services)
+        {
+            var settings = services.GetOptions<MongoDbSettings>(nameof(MongoDbSettings));
+            if (settings == null || string.IsNullOrEmpty(settings.ConnectionString))
+            {
+                throw new ArgumentNullException("DatabaseSetting is empty!");
+            }
+
+            var databaseName = settings.DatabaseName;
+            var connectionString = settings.ConnectionString + "/" + databaseName + "?authSource=admin";
+            return connectionString;
+        }
+    }
+}
---
+++ Web-Microservice/src/Services/Inventory.Grpc/Repositories/InventoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.Commons;
+using Inventory.Grpc.Entities;
+using Inventory.Grpc.Repositories.Interfaces;
+using MongoDB.Driver;
+using Shared.Configurations;
+
+namespace Inventory.Grpc.Repositories
+{
+    public class InventoryRepository : MongoDbRepository<InventoryEntry>, IInventoryRepository
+    {
+        public InventoryRepository(IMongoClient client, MongoDbSettings setting) : base(client, setting)
+        {
+        }
+
+        public async Task<int> GetStockQuantity(string itemNo) => Collection.AsQueryable()
+            .Where(x => x.ItemNo == itemNo)
+            .Sum(x => x.Quantity);
+    }
+}
